80178d2a9b933b439f81782329728336e890a646
soheila <soheilalotfi@ymail.com>
2019-10-25 14:38:24 -0400

    create App.js

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
+
+function App() {
+
+    <Router>
+        <div>
+            <Nav />
+            <Jumbotron />
+            <Switch>
+                <Route exact path="/" component={Search} />
+                <Route exact path="/saved" component={Saved} />
+                <Route component={NoMatch} />
+            </Switch>
+        </div>
+    </Router>
+
+}
+
+export default App;
