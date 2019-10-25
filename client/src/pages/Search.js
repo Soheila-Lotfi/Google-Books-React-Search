f2108ae2cd2aaf2da2744fa091de88f8e921729e
soheila <soheilalotfi@ymail.com>
2019-10-25 18:58:30 -0400

    create search.js structure

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,3 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+
+class Search extends Component {
+
+    state = {
+
+    };
+
+    render() {
+
+        return (
+
+            <Container fluid>
+
+                <Row>
+                    <h1>Book Search</h1>
+                    <h3>Search</h3>
+                    <Form>
+                        <input></input>
+                        <button></button>
+                    </Form>
+                </Row>
+                <Row>
+                    <List>
+
+                    </List>
+                </Row>
+
+
+            </Container>
+
+
+
+
+        )
+
+    }
+
+
+}
 
 
