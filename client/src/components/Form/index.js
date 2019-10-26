cb7ac5d057455cc06f9d000dc9367bf5337b6c96
soheila <soheilalotfi@ymail.com>
2019-10-25 20:08:24 -0400

    wite input and button for the search page

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,11 +3,11 @@ import React from React;
 
 export function Input(props) {
 
-    <input value={props.search} type="text" placeholder="Book title" onChange={props.handleInputChange}>
+    <input {...props}>
     </input>
 
 }
 
 export function Button(props) {
-    <button type="submit" onClick={props.handleSubmitForm}>Search</button>
+    <button {...props} >Search</button>
 }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import API from "../utils/API";
 
 class Search extends Component {
 
     state = {
+        search: "",
+
 
     };
+
+    handleInputChange = (event) => {
+        const title = event.target.value
+        this.setState({ search: title })
+
+    };
+
+    handleSubmitForm = () => {
+
+        API.getBooks(search).then(res => result = res).catch(error => console.log(error));
+
+    }
 
     render() {
 
@@ -16,8 +31,15 @@ class Search extends Component {
                     <h1>Book Search</h1>
                     <h3>Search</h3>
                     <form>
-                        <Input></Input>
-                        <Button></Button>
+                        <Input
+                            value={this.state.search}
+                            type="text"
+                            placeholder="Book title"
+                            onChange={this.handleInputChange}>
+                        </Input>
+                        <Button
+                            type="submit"
+                            onClick={this.handleSubmitForm}></Button>
                     </form>
                 </Row>
                 <Row>
